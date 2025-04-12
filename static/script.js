361af669ef165a1a671e3386ab08b63f7516adc2
okuboshigeto <okuboshigeto@gmail.com>
2025-04-12 19:35:14 +0900

    タブ切り替え機能の実装

--- static/script.js
+++ static/script.js
@@ -4,17 +4,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const tabaccoContent = document.getElementById('tabacco-content');
     const shishaContent = document.getElementById('shisha-content');
 
+    // タブ切り替えの動作（遅延を含めてopacityのアニメーションを正常に表示させる）
+    function showContent(contentToShow, contentToHide) {
+        contentToHide.style.opacity = '0';
+        setTimeout(() => {
+            contentToHide.style.display = 'none';
+            contentToShow.style.display = 'block';
+            setTimeout(() => {
+                contentToShow.style.opacity = '1';
+            }, 10); // 少し遅延を入れてopacityを変更（10ms）
+        }, 500); // opacityのトランジション時間と一致（500ms）
+    }
+
     tabaccoTab.addEventListener('click', function() {
-        tabaccoContent.style.display = 'block';
-        shishaContent.style.display = 'none';
+        showContent(tabaccoContent, shishaContent);
     });
 
     shishaTab.addEventListener('click', function() {
-        shishaContent.style.display = 'block';
-        tabaccoContent.style.display = 'none';
+        showContent(shishaContent, tabaccoContent);
     });
 
     // 初期表示設定
     tabaccoContent.style.display = 'block';
+    tabaccoContent.style.opacity = '1';
     shishaContent.style.display = 'none';
 });
